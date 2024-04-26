fb2c28dc2f91c358992fe5bf6fe4fefd9924bafe
Al3x3x <166667273+Al3x3x@users.noreply.github.com>
2024-04-25 22:04:43 -0600

    End Scroll website

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -1,20 +1,29 @@
-import React from 'react'
-import "./Hero.css"
-import profile_img from '../../assets/profile_img.png'
-
+import React from "react";
+import "./Hero.css";
+import profile_img from "../../assets/profile_img.png";
+import AnchorLink from "react-anchor-link-smooth-scroll";
 const Hero = () => {
   return (
-    <div className='hero'>
-     <img src={profile_img} alt="" />
-     <h1> <span>Hi, my name is Alexis</span></h1>
-     <p>I am a backed developer from Coahuila, Mexico passionate about competitive programming and cybersecurity ❤️</p>
-     <div className="hero-action">
-     <div className="hero-connect">Connect with me</div>
-     <div className="hero-resume">My resume</div>
-     </div>
-    
+    <div id="home" className="hero">
+      <img src={profile_img} alt="" />
+      <h1>
+        {" "}
+        <span>Hi, my name is Alexis</span>
+      </h1>
+      <p>
+        I am a backed developer from Coahuila, Mexico passionate about
+        competitive programming and cybersecurity ❤️
+      </p>
+      <div className="hero-action">
+        <div className="hero-connect">
+          <AnchorLink className="anchor-link" offset={50} href="#contact">
+            Connect with me{" "}
+          </AnchorLink>
+        </div>
+        <div className="hero-resume">My resume</div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
