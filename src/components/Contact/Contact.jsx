ece9c3de8043a1c7d1fe131f2266bc8551034066
Al3x3x <166667273+Al3x3x@users.noreply.github.com>
2024-04-25 21:25:54 -0600

    End Footer

--- src/components/Contact/Contact.jsx
+++ src/components/Contact/Contact.jsx
@@ -1,12 +1,50 @@
 import React from "react";
 import "./Contact.css";
-import theme_pattern from "../../assets/theme_pattern";
+import theme_pattern from "../../assets/theme_pattern.svg";
+import mail_icon from "../../assets/mail_icon.svg";
+import location_icon from "../../assets/location_icon.svg";
+import call_icon from "../../assets/call_icon.svg";
+
 const Contact = () => {
   return (
     <div className="contact">
       <div className="contact-title">
         <h1>Get in touch</h1>
         <img src={theme_pattern} alt="" />
+      </div>
+      <div className="contact-section">
+        <div className="contact-left">
+          <h1>Lets talk</h1>
+          <p>Im currently avaliable to work </p>
+          <div className="contact-details">
+            <div className="contact-detail">
+              <img src={mail_icon} alt="" />
+              <p>martinalexiscastanedabarajas@gmail.com</p>
+            </div>
+            <div className="contact-detail">
+              <img src={call_icon} alt="" /> <p>+528714184971</p>
+            </div>
+            <div className="contact-detail">
+              <img src={location_icon} alt="" /> <p>Torreon Coahuila, Mexico</p>
+            </div>
+          </div>
+        </div>
+        <form className="contact-rigth">
+          <label>Your Name</label>
+          <input type="text" placeholder="Enter your name" name="name" />
+          <label>Your Email</label>
+          <input type="email" placeholder="Enter your email" name="email" />
+          <label>Write yout message here</label>
+          <textarea
+            name="message"
+            rows="8"
+            placeholder="Enter your message"
+          ></textarea>
+          <button type="submit" className="contact-submint">
+            {" "}
+            Submit now
+          </button>
+        </form>
       </div>
     </div>
   );
